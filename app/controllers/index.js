fe0ac1d26cc95b7af3ad4bc4c02d0a82cc95f35a
preslavb <preslavb@abv.bg>
2017-09-12 14:48:24 +0100

    Initial Commit
    Not all the code has been reviewed yet, but it is functional

---
+++ app/controllers/index.js
@@ -0,0 +1,69 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  //Store the outside and inside surface references from model for use in the template by direct reference
+  outsideSurface: Ember.computed('model', function() {
+    return this.get('model').objectAt(0);
+  }),
+  insideSurface: Ember.computed('model', function() {
+    return this.get('model').objectAt(1);
+  }),
+  
+  //Calculate the total resistivity every time model.length changes
+  totalResistivity: Ember.computed('model.length', function() {
+    let total = 0;
+
+    for (var i = this.get('model').get('length') - 1; i >= 0; i--) {
+      total += this.get('model').objectAt(i).get('resistivityFinal');
+    }
+
+    return total.toFixed(3);
+  }),
+  
+  //Calculate the U-Value every time model.length changes
+  uValue: Ember.computed('model.length', function() {
+    return parseFloat(1/this.get('totalResistivity')).toFixed(3);
+  }),
+
+  //Create a Store record of a new material using the values from the form
+  actions: {
+    submitFormData(name, thickness, k_value) {
+      //Get all of the materials in model
+      let recordsToCompare = this.store.peekAll('material');
+      //Bool tracking whether a match was found
+      let recordAlreadyInModel = false;
+
+      //Only check the records for a match if there are any records in the model
+      if(recordsToCompare.get('length') != 0)  {
+        //Loop through all records and compare them to the one being added. If it is a complete match, then set the tracking bool to true
+        for (var i = 0 ; i < recordsToCompare.get('length'); i++) {          
+          let recordToCompare = recordsToCompare.objectAt(i);
+
+          if (recordToCompare.get('name') == name && recordToCompare.get('thickness') == thickness && 
+              recordToCompare.get('k_value') == k_value) {
+            recordAlreadyInModel = true;
+          }
+        }
+      }
+
+      //If there were no matches found, add the material to the model
+      if(!recordAlreadyInModel)  {
+        this.store.createRecord('material', {
+        name: name,
+        thickness: thickness,
+        k_value: k_value
+        });
+      }
+      else  {
+        alert("Record already saved in materials");
+      }
+    },
+
+    deleteMaterial(materialID)  {
+      //Store a local copy of all materials currently in the Data Store, then delete the one corresponding to the ID of the component pressed
+      let materials = this.store.peekAll('material');
+
+      materials.objectAt(materialID).destroyRecord();
+    }
+  }
+});
---
+++ app/models/material.js
@@ -0,0 +1,22 @@
+import DS from 'ember-data';
+
+//Model for the materials
+export default DS.Model.extend({
+  name: DS.attr(),
+  thickness: DS.attr('number'),
+  k_value: DS.attr('number'),
+  resistivity: DS.attr('number'),
+  
+  //Calculate the resistivity of the material and round it down to the 3rd digit after the dot
+  resistivityFinal: Ember.computed('thickness', 'k_value', 'resistivity', function()  {
+  	if(this.get('resistivity') == undefined){
+  	  return parseFloat(parseFloat(this.get('thickness')/this.get('k_value')).toFixed(3));
+  	}
+  	else{
+  	  return (this.get('resistivity'));
+  	}
+  }),
+  
+  //Used for rendering purposes (not drawing a delete button for constants)
+  is_const: DS.attr('boolean', { defaultValue: false})
+});
