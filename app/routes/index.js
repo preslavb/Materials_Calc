21da021ba983564a07850e69d127a61cae59e248
Ryan Petrucci <ryan@artisancodesmith.co.uk>
2017-09-13 17:15:14 +0100

    Comments

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  //Always return the full list of materials into "model" (update when there is a change in the collection)
-  model()  {
+
+  // Always return the full list of materials into "model" (update when there is a change in the collection)
+  model()
+  {
     let modelToReturn = this.store.peekAll('material');
 
     return modelToReturn;
