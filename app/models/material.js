7317c7a7d55f487b1e26419a08ab93a28b827efc
Ryan Petrucci <ryan@artisancodesmith.co.uk>
2017-09-13 17:28:39 +0100

    Last Fix!! Read on..
    All this may seem like a lot to take in, in summary:
    
    - Use 2 (actual) spaces per tab
    
    - Unless the first line in a function comments are preceded by a line
    break and have a space after the ‘//‘
    
    - Taught you some handy ES6 stuff!
    
    - camelCase and spinal-case for variables and CSS classes
    
    - Opening brackets of functions on a new line
    
    - Stick to the code hierarchy and use line breaks where necessary on
    table cells as described (not needed if single-line cells)
    
    - Avoid deprecated HTML attributes
    
    Think that’s about it.
    
    All of this should hopefully make you enjoy the look and ease of
    navigating your code - it does for me and I hope you take it on as good
    criticism, your work is brilliant thus-far - well done!! :D

--- app/models/material.js
+++ app/models/material.js
@@ -1,22 +1,24 @@
 import DS from 'ember-data';
 
-//Model for the materials
+// Model for the materials
 export default DS.Model.extend({
   name: DS.attr(),
   thickness: DS.attr('number'),
-  k_value: DS.attr('number'),
+  kValue: DS.attr('number'),
   resistivity: DS.attr('number'),
-  
-  //Calculate the resistivity of the material and round it down to the 3rd digit after the dot
-  resistivityFinal: Ember.computed('thickness', 'k_value', 'resistivity', function()  {
-  	if(this.get('resistivity') == undefined){
-  	  return this.get('thickness')/this.get('k_value');
+
+  // Calculate the resistivity of the material and round it down to the 3rd digit after the dot
+  resistivityFinal: Ember.computed('thickness', 'kValue', 'resistivity', function()
+  {
+  	if (this.get('resistivity') === undefined){
+  	  return this.get('thickness') / this.get('kValue');
   	}
-  	else{
+
+  	else {
   	  return this.get('resistivity');
   	}
   }),
-  
-  //Used for rendering purposes (not drawing a delete button for constants)
-  is_const: DS.attr('boolean', { defaultValue: false})
+
+  // Used for rendering purposes (not drawing a delete button for constants)
+  isConst: DS.attr('boolean', { defaultValue: false })
 });
